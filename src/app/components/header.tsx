7796f49a7d82f14ece87270dd9a953268934e245
Thomas <monmousseau.thomas@gmail.com>
2024-06-14 14:24:54 +0200

    fix header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,104 +1,73 @@
+// Header.tsx
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
-const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+const Header: React.FC = () => {
+  const [isNavVisible, setIsNavVisible] = useState(false);
+  const pathname = usePathname();
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
+  const toggleNav = () => {
+    setIsNavVisible(!isNavVisible);
+    if (!isNavVisible) {
+      document.body.classList.add("no-scroll");
+    } else {
+      document.body.classList.remove("no-scroll");
+    }
   };
 
+  const closeNav = () => {
+    setIsNavVisible(false);
+    document.body.classList.remove("no-scroll");
+  };
+
+  const isActiveLink = (path: string) =>
+    pathname === path ? "activeLink" : "";
+
   return (
-    <header className="max-w-7xl mx-auto flex justify-between items-center py-6 px-12 bg-main-color relative">
-      <div>
-        <Link href="/" legacyBehavior>
-          <a>
-            <Image src={"/TRIPSAGE.svg"} alt="Logo" width={80} height={80} />
+    <header className="header">
+      <Link href="/" passHref>
+        <Image
+          alt="logo"
+          src="/TRIPSAGE.svg"
+          className="header__logo"
+          width={50}
+          height={50}
+        />
+      </Link>
+      <div
+        className={`nav-burger ${isNavVisible ? "open" : ""}`}
+        onClick={toggleNav}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <nav className={`header__nav ${isNavVisible ? "open" : ""}`}>
+        <Link legacyBehavior href="/" passHref>
+          <a className={isActiveLink("/")} onClick={closeNav}>
+            Home
           </a>
         </Link>
-      </div>
-      <nav className="text-neutral-100 hidden lg:flex">
-        <ul className="flex gap-4">
-          <li>
-            <Link href="#demo" legacyBehavior>
-              <a className="hover:text-neutral-300 transition-colors">Demo</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="#pricing" legacyBehavior>
-              <a className="hover:text-neutral-300 transition-colors">
-                Pricing
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="#contact" legacyBehavior>
-              <a className="hover:text-neutral-300 transition-colors">
-                Contact
-              </a>
-            </Link>
-          </li>
-        </ul>
+        <Link legacyBehavior href="#Pricing" passHref>
+          <a className={isActiveLink("#Pricing")} onClick={closeNav}>
+            Pricing
+          </a>
+        </Link>
+        <Link legacyBehavior href="#demo" passHref>
+          <a className={isActiveLink("#demo")} onClick={closeNav}>
+            Demo
+          </a>
+        </Link>
+        <Link legacyBehavior href="#Contact" passHref>
+          <a className={isActiveLink("#Contact")} onClick={closeNav}>
+            Contact
+          </a>
+        </Link>
       </nav>
-      <button
-        className="lg:hidden text-neutral-100"
-        onClick={toggleMenu}
-        aria-label="Toggle menu"
-      >
-        <svg
-          className="w-6 h-6"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M4 6h16M4 12h16m-7 6h7"
-          />
-        </svg>
-      </button>
-      {isMenuOpen && (
-        <nav className="absolute top-16 left-0 w-full bg-main-color text-neutral-100 lg:hidden">
-          <ul className="flex flex-col items-center gap-4 py-4">
-            <li>
-              <Link href="#demo" legacyBehavior>
-                <a
-                  className="hover:text-neutral-300 transition-colors"
-                  onClick={toggleMenu}
-                >
-                  Demo
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="#pricing" legacyBehavior>
-                <a
-                  className="hover:text-neutral-300 transition-colors"
-                  onClick={toggleMenu}
-                >
-                  Pricing
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="#contact" legacyBehavior>
-                <a
-                  className="hover:text-neutral-300 transition-colors"
-                  onClick={toggleMenu}
-                >
-                  Contact
-                </a>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      )}
     </header>
   );
 };
